47f27fe7d72eafb8d7e8d7f9d5b6513ad330281f
Daniel Tura <daniel.tura@live.de>
2019-06-20 23:48:39 +0200

    responsive tutorial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import Responsive from "./components/responsiveTutorial/Responsive";
+import styles from "./components/responsiveTutorial/styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Niyousha will das sehen
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Responsive />
+      </div>
+    );
+  }
 }
 
 export default App;
